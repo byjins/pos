d23351133f44070f6e35a1934ad8769642766f71
byjins <ppwm111@naver.com>
2024-10-03 22:09:31 +0900

    feat: 폰트 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class", // or 'media' if you prefer automatic OS-based detection
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
