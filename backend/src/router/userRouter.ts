09a3556af73007fb8fc3d105d195dc350b1f4eb2
Jinmo <71693517+wlsah0814@users.noreply.github.com>
2024-10-09 15:08:28 +0900

    회원가입 http 메서드 변경

--- backend/src/router/userRouter.ts
+++ backend/src/router/userRouter.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const router: Router = express.Router();
 
 router.get('/refresh', VerifyRefresh);
-router.get('/register', Register); // 회원가입
+router.post('/register', Register); // 회원가입
 router.post('/login', Login); // 로그인
 router.post('/logout', Logout) // 로그아웃
 
